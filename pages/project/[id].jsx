51f945387e8e0f0f05680b2453d64a3c0455df6d
paulantoine2 <paul.antoine2@gmail.com>
2020-05-23 19:50:16 +0200

    Handle 404 Error on project

--- pages/project/[id].jsx
+++ pages/project/[id].jsx
@@ -3,24 +3,27 @@ import { Client } from "../../prismic-configuration";
 import Layout from "../../components/Layout";
 import Fade from "react-reveal/Fade";
 import Hero from "../../components/Hero";
+import Error from "next/error";
 
 export default class ProjectPage extends React.Component {
-  static async getInitialProps(context) {
-    const { id } = context.query;
-    const project = await Client(context.req).getByID(id);
+  static async getInitialProps({ req, res, query }) {
+    const { id } = query;
+    const project = await Client(req).getByID(id);
+    if (!project) res.statusCode = 404;
     return {
-      project: project.data,
+      project,
     };
   }
 
   render() {
     const { project } = this.props;
+    if (!project) return <Error statusCode="404" />;
     return (
       <Layout>
-        <Hero project={project} className="Project__hero" />
+        <Hero project={project.data} className="Project__hero" />
         <section className="Project">
           <Fade>
-            {project.pictures.map((p) => (
+            {project.data.pictures.map((p) => (
               <div className="block-img">
                 <img src={p.picture.url} alt={p.picture.alt} />
               </div>
